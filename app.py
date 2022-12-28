f238244eb2d29d67ca627f52e2743dc611f12be2
samfernandes1 <sam.fernandes@speedio.com.br>
2022-12-28 08:44:36 -0300

    ajuste para envio da mensagem pelo bot

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from flask import (
 
 from decouple import config
 from auth import auth
+from message import (
+    messagem_erro,
+    messagem_sucesso
+)
+
+import telegram
 
 
 app = Flask(__name__)
@@ -15,8 +21,21 @@ app = Flask(__name__)
 @auth.login_required
 def send_bot():
     data_messagem = request.get_json()
+    try:
+        if data_messagem.get('erro'):
+            mensagem = messagem_erro(data_messagem['erro'])
+            bot = telegram.Bot(token=config('KEY_BOT'))
+            bot.send_message(chat_id=config('CHAT_ID'), text=mensagem)
+        else:
+            mensagem = messagem_sucesso(data_messagem['sucesso'])
+            bot = telegram.Bot(token=config('KEY_BOT'))
+            bot.send_message(chat_id=config('CHAT_ID'), text=mensagem)
 
-    return make_response({'envio': 'sucesso'}, 200)
+        return make_response({'envio': 'sucesso'}, 200)
+
+    except Exception as e:
+        error = str(e)
+        return make_response({'error': f'Error {error}'}, 400)
 
 
 if __name__ == '__main__':
---
+++ message.py
@@ -0,0 +1,11 @@
+
+def messagem_erro(mensagem):
+    mensagem_atual = f"""############## ERROR ############### \
+    \n{mensagem}\n############ FIM ERROR #############"""
+    return mensagem_atual
+
+
+def messagem_sucesso(mensagem):
+    mensagem_atual = f"""############## SUCESSO ############### \
+    \n{mensagem}\n############ FIM SUCESSO #############"""
+    return mensagem_atual
